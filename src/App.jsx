b47ad8196e8b61ba3aefd21f13b954a7e9bd7510
Scott Percy <spercy@buildtechnologygroup.com>
2025-06-29 18:20:29 -0400

    Testing deployment script

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div class="container d-flex align-items-center justify-content-center min-vh-100">
-        <h1>Coming soon...</h1>
+        <h1>Coming soon...3</h1>
       </div>
     </>
   )
