008e7728b129fa9d7bb5d9ae66a5e91a5a8270d3
Scott Percy <spercy@buildtechnologygroup.com>
2025-06-29 19:48:24 -0400

    Testing out environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+const testKey = import.meta.env.VITE_TEST_KEY;//process.env.REACT_APP_TEST_KEY;
 
 function App() {
   return (
     <>
       <div class="container d-flex align-items-center justify-content-center min-vh-100">
-        <h1>Coming soon...3</h1>
+        <h1>Coming soon...{testKey}</h1>
       </div>
     </>
   )
